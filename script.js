7fc0f3a697491a59e65e1acdc5b3436b9a871b06
rioobod <akanbilola@gmail.com>
2022-01-16 11:00:23 -0600

    created array variables for possible password characters

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+  // variables for possible password 
+  var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var owasp = ["!", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "|", "/", "[", "]", "`", "~", "<", ",", ".", ">", "/", "?"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
