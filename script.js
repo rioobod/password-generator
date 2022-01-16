1403ffd36533044b6851ddf7fca344b206e1d538
rioobod <akanbilola@gmail.com>
2022-01-16 11:15:16 -0600

    function to check password length

--- script.js
+++ script.js
@@ -1,30 +1,131 @@
 // Assignment code here
-  // variables for possible password 
-  var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var owasp = ["!", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "|", "/", "[", "]", "`", "~", "<", ",", ".", ">", "/", "?"];
+// variables for possible password
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var owasp = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "=",
+  "+",
+  "{",
+  "}",
+  "|",
+  "/",
+  "[",
+  "]",
+  "`",
+  "~",
+  "<",
+  ",",
+  ".",
+  ">",
+  "/",
+  "?",
+];
 
-    // define the min and max of the password length.
-    var min = 8;
-    var max = 128;
+// define the min and max of the password length.
+var min = 8;
+var max = 128;
 
+// define an empty array to contain all the chosen parameters.
+var passwordArray = [];
 
-  // define an empty array to contain all the chosen parameters.
-  var passwordArray = [];
+// create a var to hold the length of the desired password.
+var passwordLengthNum;
 
-  // create a var to hold the length of the desired password.
-  var passwordLengthNum;
+// create a function to prompt the user to define a length for the password.
+function getPasswordLength() {
+  var passwordLength;
+  passwordLength = window.prompt(
+    "Please choose a password length between a minimum of 8 and a maximum of 128 characters?"
+  );
+  passwordLengthNum = parseInt(passwordLength);
+  checkPasswordLength();
+  // console.log(passwordLengthNum);
+}
 
-    // create a function to prompt the user to define a length for the password.
-    function getPasswordLength(){
-      var passwordLength;
-      passwordLength = window.prompt("Please choose a password length between a minimum of 8 and a maximum of 128 characters?");
-      passwordLengthNum = parseInt(passwordLength);
-      console.log(passwordLengthNum);
-    };
-  
+getPasswordLength();
+
+// create a function to check the length of the password.
+function checkPasswordLength() {
+  if (passwordLengthNum < min || passwordLengthNum > max) {
+    // getPasswordLength();
+    console.log("incorrect");
+    lengthCorrect = false;
+  } else {
+    // return passwordLength;
+    lengthCorrect = true;
+    console.log(passwordLengthNum);
+  }
+
+  if (lengthCorrect === false) {
     getPasswordLength();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -35,7 +136,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
